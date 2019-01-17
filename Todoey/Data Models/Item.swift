01b5d8d013268b58ff44f4a0c61694808b803983
Me 018 <tony@tonys-mac.local>
2019-01-17 10:38:24 +0200

    convert to mvc pattern

---
+++ Todoey/Data Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by tony on 1/16/19.
+//  Copyright © 2019 mekoo. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var title:String = ""
+    var done:Bool=false
+}
+
